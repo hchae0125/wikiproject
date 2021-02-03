68d6ba5a32050a3677b51b4795236e53c781e361
hchae0125 <dolemi25@gmail.com>
2021-02-02 23:19:54 -0500

    Back End Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './components/main'
+import axios from 'axios'
 
 function App() {
+  const hitBackend = () => {
+    axios.get('/test')
+    .then((response) => {
+    console.log(response.data)
+    })
+    }
   return (
     <div className="App">
       Hello, World!
+      <Main />
+      <button onClick={hitBackend}>Send request</button>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,27 @@
 const express = require('express');
-const cors = require('cors');
+const mysql = require('mysql');
 const app = express();
+const cors = require('cors');
 app.use(cors());
+
+var connection = mysql.createConnection({
+    host:'wikiprojectdb.cju7fv11u0ey.us-east-2.rds.amazonaws.com',
+    user:'admin',
+    password:'Gksmf0410!',
+    port:'3306'
+});
+connection.connect(function(err) {
+    if (err){
+        console.error('Database connection failed' + err.stack);
+        return;
+    }
+    console.log('connected to database');
+})
+app.get('/test', (req, res) => {
+    console.log(req);
+    res.send('Welcome to the Backend');
+})
+
 app.listen(4000, ()=>{
     console.log('running on port 4000');
-})
+});
